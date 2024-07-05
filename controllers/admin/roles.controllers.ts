18e625f8b4090ff7b6d61e07804843f26c3f4378
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-07-05 23:48:54 +0700

    update and implement add featured

--- controllers/admin/roles.controllers.ts
+++ controllers/admin/roles.controllers.ts
@@ -1,14 +1,17 @@
 
 import {Request, Response} from 'express';
-import Roles from '../../models/roles.model';
+import Role from '../../models/roles.model';
 import { btnStatus } from '../../helpers/status.helper';
+import system from '../../config/system'; 
+
+const prefixAdmin = system.prefixAdmin
+//[GET] "/admin/roles"
 export const index = async (req: Request, res: Response) :Promise<void>  =>{
 
 
     //list btn filter 
     const listBtn = btnStatus(req);
-
-    const roles = await Roles.find({
+    const roles = await Role.find({
         deleted: false
     })
     res.render("admin/pages/roles/index.pug",{
@@ -16,4 +19,22 @@ export const index = async (req: Request, res: Response) :Promise<void>  =>{
         listBtn: listBtn,
         roles: roles
     })
+}
+//[GET] "/admin/roles/add"
+export const add = async (req: Request,res: Response) : Promise<void> =>{
+    res.render("admin/pages/roles/create.pug",{
+        activePages: "roles"
+    });
+}
+//[POST] "/admin/roles/add"
+export const addPost = async (req: Request, res: Response) :Promise<void>  =>{4
+    try {
+        const role = new Role(req.body);
+        await role.save();
+        req.flash('success_msg','Thêm nhóm quyền thành công')
+        res.redirect(`/${prefixAdmin}/roles`);
+    } catch (error) {
+        req.flash('error_msg','Thêm nhóm quyền thất bại')
+        res.redirect("back");
+    }
 }
--- router/admin/roles.router.ts
+++ router/admin/roles.router.ts
@@ -2,5 +2,6 @@ import { Router } from "express";
 const router = Router();
 import * as controller from '../../controllers/admin/roles.controllers';
 router.get("/",controller.index);
-
+router.get("/add",controller.add);
+router.post("/add",controller.addPost);
 export default router;
