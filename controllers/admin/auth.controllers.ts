7c7d7ff9d5b6a353ac0f296f601e670aafd00ba5
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-07-13 10:52:15 +0700

    implement login account featured

--- controllers/admin/auth.controllers.ts
+++ controllers/admin/auth.controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import {compare} from 'bcrypt';
 import Account from '../../models/accounts.model';
 import system from '../../config/system';
 const prefixAdmin = system.prefixAdmin;
@@ -7,11 +8,31 @@ export const login = (req: Request, res: Response) =>{
     res.render("admin/pages/auth/login.pug")
 }
 //[POST] "/admin/auth/login"
-export const loginPost = (req: Request, res: Response) =>{
-    const email = req.body.email;
-    const password = req.body.password;
-    const isExistsAccount = Account.findOne({
-        email: email
-    })
-    res.redirect(`/${prefixAdmin}/dashboard`)
+export const loginPost = async (req: Request, res: Response) =>{
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+    try {
+        const isExistsAccount = await Account.findOne({
+            email: email
+        })
+    
+        if(!isExistsAccount){
+            req.flash('error_msg','Email Không Tồn Tại');
+            res.redirect("back");
+            return;
+        }
+        const isExistsPassword = compare(isExistsAccount.password,password);
+        if(!isExistsPassword){
+            req.flash('error_msg','Mật khẩu của bạn đã bị sai');
+            res.redirect("back");
+            return;
+        }
+        res.cookie("tokenAccount",isExistsAccount.token);
+        res.redirect(`/${prefixAdmin}/dashboard`)
+    } catch (error) {
+        console.error(error);
+        req.flash('error_msg','Lỗi Không xác định');
+        res.redirect("back");
+    }
+    
 }
